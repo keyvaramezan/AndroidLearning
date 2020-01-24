79a35ebaf91618dca39136a660c459607a453203
unknown <keyvanramezan@gmail.com>
2020-01-25 00:15:55 +0330

    Edite Layout for Test

--- app/src/main/java/ir/example/androidlearning/MainActivity.java
+++ app/src/main/java/ir/example/androidlearning/MainActivity.java
@@ -2,8 +2,10 @@ package ir.example.androidlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,33 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Tag", "onCreate: ");
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Tag", "onStart:");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("Tag", "onResume:");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Tag", "onPause:");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Tag", "onStop:");
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Tag", "onDestroy:");
-    }
 }
